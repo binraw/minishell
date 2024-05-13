74912f90bce7989c4bbae368c9465baa347eb262
Robin Truvelot <rtruvelo@z3r12p5.42lyon.fr>
2024-05-13 16:09:13 +0200

    add debut de unset

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: rtruvelo <rtruvelo@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 14:15:19 by rtruvelo          #+#    #+#             */
-/*   Updated: 2024/05/13 15:39:25 by rtruvelo         ###   ########.fr       */
+/*   Updated: 2024/05/13 16:08:45 by rtruvelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,17 +128,46 @@ int		remove_env_value(t_data *data, char *name_value)
 // 	return (data->env_node);
 // }
 //
-// int remove_node(t_node_env *current_node, t_node_env *second_node, t_node_env *third_node)
-// {
-// 	if (current_node == NULL || second_node == NULL || third_node == NULL)
-// 	{
-// 		// Gérer les cas où les nœuds sont NULL
-// 		return (-1);
-// 	}
-// 	free(second_node);
-// 	current_node->next = third_node;
-// 	return (0);
-// }
+//
+
+int	unset_command(t_data *data, char *value)
+{
+	t_node_env *second_node;
+	t_node_env *third_node;
+
+	second_node = data->env_node->next;
+	third_node = second_node->next;
+
+	while (third_node)
+	{
+
+		if (ft_strncmp(second_node->content, value, ft_strlen(value)) == 0)
+		{
+				
+			remove_node(data->env_node, second_node, third_node);
+			break ;
+		}
+
+		data->env_node = data->env_node->next;
+		second_node = second_node->next;
+		third_node = third_node->next;
+	}
+	return (0);
+}
+
+
+
+int remove_node(t_node_env *current_node, t_node_env *second_node, t_node_env *third_node)
+{
+	if (current_node == NULL || second_node == NULL || third_node == NULL)
+	{
+		// Gérer les cas où les nœuds sont NULL
+		return (-1);
+	}
+	free(second_node);
+	current_node->next = third_node;
+	return (0);
+}
 
 
 void	screen_export(t_data *data, int fd)
